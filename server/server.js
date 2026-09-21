930565ac88d4598e26b50235dbfbc3bfb5675df3
Arpan Pakhira <140741601+Arpanpakhira@users.noreply.github.com>
2026-09-21 11:21:18 +0530

    Update CORS middleware configuration
    Configure CORS to allow requests from specific origin with credentials.

--- server/server.js
+++ server/server.js
@@ -29,7 +29,10 @@ const io = new Server(server, {
 });
 
 // Middleware
-app.use(cors());
+app.use(cors({
+origin:"https://ryzo-socialmediaplatform-client.onrender.com",
+  credentials:true
+));
 app.use(express.json());
 
 // Routes
